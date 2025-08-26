35bbd677fac5ee430749244a6821f2444f800546
Owais-Khan <owais.khan@alumni.utoronto.ca>
2025-08-26 10:16:56 -0400

    updated

--- app.py
+++ app.py
@@ -1,87 +1,45 @@
 from trame.app import get_server
-from trame.ui.vuetify3 import SinglePageLayout, VAppLayout
-from trame.widgets import vuetify3
+from trame.ui.vuetify import SinglePageLayout
+from trame.widgets import vuetify, vtk as vtk_widgets
 
+# -----------------------------------------------------------------------------
+# Trame setup
+# -----------------------------------------------------------------------------
 
-class MultiWindow:
-    def __init__(self, server=None, table_size=10):
-        self.server = get_server(server, client_type="vue3")
+server = get_server()
+state, ctrl = server.state, server.controller
 
-        # Create local variable for JS side
-        self.state.update(dict(window_hello=None, window_world=None))
-        self.state.client_only("window_hello", "window_world")
+# -----------------------------------------------------------------------------
+# Web App setup
+# -----------------------------------------------------------------------------
 
-        # Force ui creation
-        self.ui = self.ui_main
-        self.popup_hello = self.ui_hello
-        self.popup_world = self.ui_world
+state.trame__title = "VTK Rendering"
 
-    @property
-    def state(self):
-        return self.server.state
+with SinglePageLayout(server) as layout:
+    with layout.content:
+        with vuetify.VContainer(fluid=True, classes="pa-0 fill-height"):
+            with vtk_widgets.VtkView(ref="view"):
+                with vtk_widgets.VtkGeometryRepresentation():
+                    vtk_widgets.VtkAlgorithm(
+                        vtk_class="vtkConeSource", state=("{ resolution }",)
+                    )
 
-    @property
-    def ctrl(self):
-        return self.server.controller
-
-    @property
-    def ui_main(self):
-        with SinglePageLayout(self.server, full_height=True) as layout:
-            with layout.toolbar.clear():
-                vuetify3.VToolbarTitle("Multi Window example")
-                vuetify3.VSpacer()
-                vuetify3.VBtn(
-                    "Open Hello",
-                    disabled=("window_hello",),
-                    click="""
-                        window_hello = window.open(
-                            '/?ui=hello',
-                            target='_blank',
-                            'popup,width=200,height=200,left=10,top=10'
-                        )
-                    """,
-                )
-                vuetify3.VBtn(
-                    "Close Hello", click="window_hello.close(); window_hello = null;"
-                )
-                vuetify3.VBtn(
-                    "Open World",
-                    disabled=("window_world",),
-                    click="""
-                        window_world = window.open(
-                            '/?ui=world',
-                            target='_blank',
-                            'popup,width=200,height=200,left=100,top=100'
-                        )
-                    """,
-                )
-                vuetify3.VBtn(
-                    "Close World", click="window_world.close(); window_world = null;"
-                )
-            with layout.content:
-                with vuetify3.VContainer():
-                    with vuetify3.VCard():
-                        vuetify3.VCardTitle("Main Page")
-
-    @property
-    def ui_hello(self):
-        with VAppLayout(self.server, template_name="hello", full_height=True):
-            with vuetify3.VContainer():
-                with vuetify3.VCard():
-                    vuetify3.VCardTitle("Hello Page")
-
-    @property
-    def ui_world(self):
-        with VAppLayout(self.server, template_name="world", full_height=True):
-            with vuetify3.VContainer():
-                with vuetify3.VCard():
-                    vuetify3.VCardTitle("World Page")
-
-
-def main():
-    app = MultiWindow()
-    app.server.start()
-
+    with layout.toolbar:
+        vuetify.VSpacer()
+        vuetify.VSlider(
+            hide_details=True,
+            v_model=("resolution", 6),
+            max=60,
+            min=3,
+            step=1,
+            style="max-width: 300px;",
+        )
+        vuetify.VSwitch(
+            hide_details=True,
+            v_model=("$vuetify.theme.dark",),
+        )
+        with vuetify.VBtn(icon=True, click="$refs.view.resetCamera()"):
+            vuetify.VIcon("mdi-crop-free")
 
 if __name__ == "__main__":
-    main()
+    server.start()
